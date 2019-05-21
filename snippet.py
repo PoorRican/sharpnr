2f2c5a65afc53548417a9e721ca5cc77afd6b0a5
swe <swe@chromebook>
2019-05-20 23:05:09 -0400

    Changed reference to property for Snippet

--- snippet.py
+++ snippet.py
@@ -1,4 +1,5 @@
 from yaml import dump, load, YAMLObject
+from random import randrange
 
 
 STORAGE_FN = 'snippets.yaml'
@@ -12,22 +13,36 @@ class Reference(YAMLObject):
     def __str__(self):
         return self.reference
 
+    def __unicode__(self):
+        return u"%s" % self.reference
+
+    def __repr__(self):
+        return "%s(reference=%r)" % (
+               self.__class__.__name__, self.reference)
+
 class Snippet(YAMLObject):
     yaml_tag = u'!Snippet'
     db = []
     def __init__(self, reference, contents, version="ESV"):
-        self.reference = Reference(reference)
+        self._reference = Reference(reference)
         self.contents = contents
         self.version = version
 
         self.__class__.db.append(self)
 
+    @property
+    def reference(self):
+        return self._reference.reference
+
     def __str__(self):
         return "%s - %s" % (self.contents, self.reference)
 
+    def __unicode__(self):
+        return u"%s - %s" % (self.contents, self.reference)
+
     def __repr__(self):
         return "%s(reference=%r, contents=%r, version=%r)" % (
-               self.__class__.__name__, self.reference, self.contents, self.version)
+               self.__class__.__name__, self._reference, self.contents, self.version)
 
     @classmethod
     def save(cls, fn=STORAGE_FN):
@@ -38,3 +53,12 @@ class Snippet(YAMLObject):
     def read(cls, fn=STORAGE_FN):
         with open(fn, 'r') as f:
             cls.db = load(f)
+
+    @classmethod
+    def random(cls):
+        _max = len(cls.db)
+        # TODO: implement array of previous selections to prevent repeats
+        return cls.db[randrange(_max)]
+
+
+Snippet.read(STORAGE_FN)
